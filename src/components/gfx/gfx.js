da450c472465e83a8004d49e5cf5f00d57299355
Hamza Khuswan <hamzakhuswan@outlook.om>
2020-12-12 08:12:20 +0100

    removing count down

--- src/components/gfx/gfx.js
+++ src/components/gfx/gfx.js
@@ -1,27 +1,29 @@
 import "./gfx.scss";
 
-const date = "2021/2/1";
+// const date = "2021/2/1";
 
-const updateDate = new Date(date).getTime();
+// const updateDate = new Date(date).getTime();
 
-const repeatedFuc = () => {
-    const currentDate = new Date().getTime();
-    const diff = updateDate - currentDate;
+// document.getElementById("countdown").style = "";
+
+// const repeatedFuc = () => {
+//     const currentDate = new Date().getTime();
+//     const diff = updateDate - currentDate;
     
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+//     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
     
-    document.getElementById("nextUpdate").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+//     document.getElementById("nextUpdate").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     
-    if (diff < 0) {
-        clearInterval(interval);
-        document.getElementById("nextUpdate").innerHTML = "SOON";
-    }    
-}
-const interval = setInterval(repeatedFuc, 1000);
-repeatedFuc();
+//     if (diff < 0) {
+//         clearInterval(interval);
+//         document.getElementById("nextUpdate").innerHTML = "SOON";
+//     }    
+// }
+// const interval = setInterval(repeatedFuc, 1000);
+// repeatedFuc();
 
 
 
